b8a73752a9dad48896681c981b2e5a4931c16841
Per Pettersson <Per.Pettersson@gmail.com>
2024-04-08 14:05:55 -0400

    Create README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# My dotfiles
+
+## Tools
+
+- stow
+- zellij
+- ripgrep
+- nnn
+- lazygit
+- atuin
+- zsh
+- delta
+- wezterm
+
+## Neovim
+Plugin based on [Astronvim](https://docs.astronvim.com)
--- home/.config/nvim/lua/plugins/core/oil.lua
+++ home/.config/nvim/lua/plugins/core/oil.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "stevearc/oil.nvim",
-    cmd = "Oil",
-    -- keys = { "<leader>o" },
-    keys = { "\\o" },
-    config = function()
-        require("oil").setup({
-            default_file_explorer = false,
-        })
-        vim.keymap.set("n", "<leader>o", ":Oil<CR>", { desc = "Oil" })
-    end,
-  },
+	{
+		"stevearc/oil.nvim",
+		cmd = "Oil",
+		-- keys = { "<leader>o" },
+		keys = { "-" },
+		config = function()
+			require("oil").setup({
+				default_file_explorer = false,
+			})
+			vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Oil" })
+		end,
+	},
 }
