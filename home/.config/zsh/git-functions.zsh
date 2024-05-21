a588da8e18d0eef6ea690fd6a6dfe7e05c0f35fb
pellepedro <Per.Pettersson@gmail.com>
2024-05-21 17:06:51 +0000

    updated git functions

--- home/.config/zsh/git-functions.zsh
+++ home/.config/zsh/git-functions.zsh
@@ -16,7 +16,6 @@ function newcluster() {
     rmcluster
     ${SKYRAMP_HOME}/bin/skyramp cluster create -l || true
     cp ~/.skyramp/kind-cluster.kubeconfig ~/.kube/config
-    rm -f ~/.kube/config || true
 }
 
 function makeall() {
@@ -24,6 +23,8 @@ function makeall() {
     pushd $SKYRAMP_HOME
     make all
     make build-worker
+    make ecr-login
+    make upload-worker
     make build-so
     make package-pip
     make pip-install
@@ -79,4 +80,32 @@ function syncso() {
     dest=~/git/letsramp/skyramp/libs/pip/src/skyramp/lib/
     rm -rf $dest/*
     rsync -av ~/git/letsramp/skyramp/bin/github.com/letsramp/* $dest
+}
+
+function zapns() {
+  # Define the array of namespaces
+  namespaces=(
+    "chaining-test"
+    "eks-test"
+    "grpc-checkout-test"
+    "grpc-test"
+    "rest-test
+    "kube-node-lease"
+    "rafay-test"
+    "rbac"
+    "rest-checkout-test"
+    "rest-test-with-no-openapi"
+    "test-rafay"
+  )
+
+  # Iterate over the array and delete each namespace with --force
+  for namespace in "${namespaces[@]}"; do
+    echo "Deleting namespace: $namespace"
+    kubectl delete namespace "$namespace" --force --grace-period=0 2>/dev/null
+    if [[ $? -eq 0 ]]; then
+      echo "Successfully deleted namespace: $namespace"
+    else
+      echo "Failed to delete namespace: $namespace (ignoring error)"
+    fi
+  done
 }
