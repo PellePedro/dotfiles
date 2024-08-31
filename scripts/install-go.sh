1706450ed5e8b8868f0ca74243e87ee432e5741c
pellepedro <Per.Pettersson@gmail.com>
2024-08-31 03:31:35 +0000

    install-go

---
+++ scripts/install-go.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -x
+GO_VERSION="1.22.6"
+GOPLS_VERSION=“latest”
+STATIC_CHECK_VERSION=“2023.1.2”
+GOLANGCI_VERSION=“v1.50.0”
+DELVE_VERSION=“latest”
+GOLANG_TAR=linux-amd64.tar.gz
+GOLANG_DOWNLOAD_URL=https://dl.google.com/go/go${GO_VERSION}.${GOLANG_TAR}
+
+if [ -L "/usr/local/bin/go" ]; then
+    sudo rm "/usr/local/bin/go"
+fi
+
+if [ -d "/usr/local/go" ]; then
+  sudo rm -rf "/usr/local/go"
+fi
+
+curl -fsSL "$GOLANG_DOWNLOAD_URL" -o golang.tar.gz
+sudo tar -C /usr/local -xzf golang.tar.gz
+rm golang.tar.gz
+sudo ln -sf /usr/local/go/bin/go /usr/local/bin/go
+sudo rm -rf ${GOLANG_TAR}
+
+
+go install github.com/jesseduffield/lazygit@latest
+go install github.com/fatih/gomodifytags@latest
+go install github.com/josharian/impl@latest
+go get -u github.com/cweill/gotests/...
+go get -u github.com/koron/iferr
+
+
+STATIC_CHECK_VERSION=“2023.1.2”
+GOLANGCI_VERSION=“v1.50.0”
+DELVE_VERSION=“latest”
+go install honnef.co/go/tools/cmd/staticcheck@v1.50.0
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.0
+go install github.com/go-delve/delve/cmd/dlv@latest
+
+go install golang.org/x/tools/gopls@”$GOPLS_VERSION”
+
+
