beb38aa9f06bad78181a5e8bc1574a73f7acc183
pellepedro <Per.Pettersson@gmail.com>
2024-08-31 03:35:40 +0000

    apt packages

---
+++ scripts/install-packages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt-get update -yq
+sudo apt-get install -y --no-install-recommends \
+btrfs-progs \
+crun \
+git \
+make \
+go-md2man \
+iptables \
+libassuan-dev \
+libbtrfs-dev \
+libc6-dev \
+libdevmapper-dev \
+libglib2.0-dev \
+libgpgme-dev \
+libgpg-error-dev \
+libprotobuf-dev \
+libprotobuf-c-dev \
+libseccomp-dev \
+libselinux1-dev \
+libsystemd-dev \
+pkg-config \
+uidmap \
+ca-certificates \
+curl            \
+fzf             \
+git             \
+nnn             \
+meld            \
+ripgrep         \
+vim             \
+zsh             \
