a2076293be9c8d999e6d4eada58a61aee63c14b3
pellepedro <Per.Pettersson@gmail.com>
2024-09-05 09:13:30 -0400

    updated packages

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
 
-sudo apt-get update -yq
-sudo apt-get install -y --no-install-recommends \
-btrfs-progs \
-crun \
-git \
-make \
-go-md2man \
-iptables \
-libassuan-dev \
-libbtrfs-dev \
-libc6-dev \
-libdevmapper-dev \
-libglib2.0-dev \
-libgpgme-dev \
-libgpg-error-dev \
-libprotobuf-dev \
-libprotobuf-c-dev \
-libseccomp-dev \
-libselinux1-dev \
-libsystemd-dev \
-pkg-config \
-uidmap \
-ca-certificates \
-curl            \
-fzf             \
-git             \
-nnn             \
-meld            \
-ripgrep         \
-vim             \
-zsh             \
+function install_ubuntu_package() {
+    sudo apt-get update -yq
+    sudo apt-get install -y --no-install-recommends \
+    pkg-config      \
+    uidmap          \
+    ca-certificates \
+    curl            \
+    fzf             \
+    git             \
+    nnn             \
+    ripgrep         \
+    vim             \
+    zsh             \
+    build-essential \
+    dnsutils        \
+    net-tools       \
+    netcat          \
+    iputils-ping    \
+    socat           \
+    jq
+}
+
+function install_arch_packages() {
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S --noconfirm \
+    pkgconf         \
+    shadow          \
+    ca-certificates \
+    curl            \
+    fzf             \
+    git             \
+    nnn             \
+    ripgrep         \
+    vim             \
+    zsh             \
+    base-devel      \
+    inetutils       \
+    net-tools       \
+    gnu-netcat      \
+    iputils         \
+    socat           \
+    jq
+}
+
+source /etc/os-release
+
+if [[ "$ID" == "ubuntu" ]]; then
+    install_ubuntu_package
+elif [[ "$ID" == "arch" ]]; then
+    install_arch_packages
+else
+    echo "The operating system is not recognized as Ubuntu or Arch Linux."
+fi
